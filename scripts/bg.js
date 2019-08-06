4512b3db801f63eaac967767ab699a987a7c7418
quinton22 <quintonhoffman22@gmail.com>
2019-08-06 04:12:35 -0700

    background parallax and better mobile views

--- scripts/bg.js
+++ scripts/bg.js
@@ -13,14 +13,17 @@ let themeColorCount = Array(chosenTheme.length).fill(limit);
 
 // TODO: add logic for toggling animation
 const shouldAnimate = true;
+// TODO: add logic for toggling parallax
+const parallax = true;
 
-// 3 - 6 circles
-const numCircles = Math.floor(Math.random() * 3) + 3;
-
+// 4 - 8 circles
+const numCircles = getIntegerBetweenInclusive(4, 8);
 createCircles(numCircles);
 
 function createCircles(n) {
 	const bg = document.getElementById('bg');
+	const bgLayers = new Array(parallax ? 2 : 1).fill(null).map(_ => document.createElement('div'));
+
 	for (let i = 0; i < n; ++i) {
 		const { position, radius, color, border_radius } = generateCircleAttributes();
 		const style = `left: ${position.x}px;
@@ -29,8 +32,21 @@ function createCircles(n) {
 							width: ${radius * 2}px;
 							border: solid ${border_radius}px ${color};`
 		const circle = `<div id="bg-circle${i}" class="${shouldAnimate ? 'bg-circle bg-circle--animated' : 'bg-circle'}" style="${style}"></div>`;
-		bg.innerHTML += circle;
+
+		if (i < n / bgLayers.length) {
+			bgLayers[0].innerHTML += circle;
+		} else {
+			bgLayers[1].innerHTML += circle;
+		}
 	}
+
+	bgLayers.forEach((layer, index) => {
+		if (parallax) {
+			layer.classList.add('parallax-bg');
+			layer.style.transform = `translateZ(${index-2}px) scale(${index+3})`;
+		}
+		bg.appendChild(layer);
+	});
 }
 
 function generateCircleAttributes() {
@@ -56,6 +72,13 @@ function generateCircleAttributes() {
 		border_radius: borderRadius
 	}
 }
+
+function getIntegerBetweenInclusive(n1, n2) {
+	if (n1 === n2) return;
+
+	return Math.floor(Math.random() * (Math.abs(n2 - n1) + 1)) + Math.min(n1, n2);
+}
+
 
 // IDEA: could do a bubbling animation replacing circles when animation ends
 //	css::	animation: 2s ease 1 alternate grow;
--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,6 @@ const myKeyAttributes = ["Student", "Software Developer", "Spikeball Enthusiast"
 
 document.getElementById('my-key-attributes').innerHTML = myKeyAttributes.join('&nbsp;&nbsp;<span class="vertical-bar text-emphasis-color"></span>&nbsp;&nbsp;');
 
-const mainCard = document.getElementsByClassName("section-card")[0];
+const homeSectionCard = document.getElementsByClassName("home-section__card")[0];
 
-mainCard.style.transform = "translateX(0)";
+homeSectionCard.style.transform = "translateX(0)";
