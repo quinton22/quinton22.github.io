78594906bbefc7e4afb022061336c4880045086e
quinton22 <quintonhoffman22@gmail.com>
2019-08-08 04:04:58 -0700

    changed transition to animation and added arrow down for next section

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,3 @@
 const myKeyAttributes = ["Student", "Software Developer", "Spikeball Enthusiast"];
 
 document.getElementById('my-key-attributes').innerHTML = myKeyAttributes.join('&nbsp;&nbsp;<span class="vertical-bar text-emphasis-color"></span>&nbsp;&nbsp;');
-
-const homeSectionCard = document.getElementsByClassName("home-section__card")[0];
-
-homeSectionCard.style.transform = "translateX(0)";
